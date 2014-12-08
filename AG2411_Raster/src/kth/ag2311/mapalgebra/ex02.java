bcccb47cef3df75b9e98944052af1317ae3dce59
nntung <nguyen.ngoc.tung.2007@gmail.com>
2014-12-08 10:19:58 +0100

    ex02 update from instruction

--- AG2411_Raster/src/kth/ag2311/mapalgebra/ex02.java
+++ AG2411_Raster/src/kth/ag2311/mapalgebra/ex02.java
@@ -14,12 +14,27 @@ public class ex02 {
 			Layer layer = new Layer(args[0], args[1]);
 			//Show it on the screen with gray scale level
 			layer.map();
-			//Show it on the screen with color
-			double[] color = {2.0,3.0};
-			layer.map(color);
+			
+			if (args.length > 2) {
+				int numberOfInterest = args.length - 2;
+				//Show it on the screen with color
+				double[] interests = new double[numberOfInterest];
+				int j = 2;
+				for (int i = 0; i<numberOfInterest; i++) {
+					try {
+						interests[i] = Double.parseDouble(args[j]);
+					} catch (NumberFormatException ex) {
+						interests[i] = layer.nullValue;
+					}
+					j++;
+				}
+					
+				layer.map(interests);
+			}
 		} else {
 			System.out.println("Too few arguments ...");
-			System.out.println("Ex: ex02 Layer_Development c:/DataJava/dev_ascii.txt ...");
+			System.out.println("Ex: ex02 Layer_Development c:/DataJava/dev_ascii.txt ");
+			System.out.println("Ex: ex02 Layer_Development c:/DataJava/dev_ascii.txt 2.0 3.0");
 		}
 	}
 }
